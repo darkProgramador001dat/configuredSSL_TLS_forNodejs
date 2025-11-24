7bba6b6c3568961da6c812415207d1898c41ddb7
darkProgramador001dat <squareroom01000010@gmail.com>
2025-11-24 02:20:06 -0300

    Update simpleServer.js

--- simpleServer.js
+++ simpleServer.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const https = require('https');
-const path = require('path');
-const fs = require('fs');
+const express = require('express');  // Biblioteca Framework Express
+const https = require('https');      // Biblioteca Auxilio para criar um servidor
+const path = require('path');        // Biblioteca para redirecionar os caminhos no servidor
+const fs = require('fs');            // Biblioteca para CRUD
 
-const app = express();
+const app = express();    // Instancia do servidor.
 
+// Criando uma instancia, utilizando o FileSystem para abrir o diretorio e acessar o SSL
 const privateKey = fs.readFileSync(path.join(__dirname, 'private-key.pem'), 'utf-8');
 const certificate = fs.readFileSync(path.join(__dirname, 'certificate.pem'), 'utf-8');
-const credentials = { key: privateKey, cert: certificate };
+const credentials = { key: privateKey, cert: certificate };    // Criando um Objeto que assegura-ra o SSL
 
+// ROTA raiz.
 app.get('/', (request, response) => {
   response.statusCode = 200;
   response.setHeader('Content-Type', 'text/html');
@@ -26,6 +28,7 @@ app.get('/', (request, response) => {
   `);
 });
 
+// Colocando o Objeto(SSL) no codigo junto ao servidor.
 https.createServer(credentials, app).listen(443, () => {
   console.log('https://localhost:443');
 });
